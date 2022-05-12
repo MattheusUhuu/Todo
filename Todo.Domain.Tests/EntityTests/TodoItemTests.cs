f250357943fea55492a504eeb4b1178813b609e2
Pedro Matheus <matt_4.6.8@outlook.com>
2022-05-12 08:39:55 -0300

    Testando o TodoItem e fazendo ele falhar, red - red, green, refactore

---
+++ Todo.Domain.Tests/EntityTests/TodoItemTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Todo.Domain.Tests.EntityTests;
+
+[TestClass]
+public class TodoItemTests
+{
+    [TestMethod]
+    public void Dado_um_novo_todo_o_mesmo_nao_pode_ser_concluido()
+    {
+        // red, green, refactore
+        Assert.Fail();
+    }
+}
