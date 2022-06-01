c074aa04191865103414dc6fcb9dabdd0b17468b
Pedro Matheus <matt_4.6.8@outlook.com>
2022-06-01 10:04:42 -0300

    Implementado os metodos MarkTodoAsDone e MarkTodoAsUndone na classe TodoHandler

--- Todo.Domain/Handlers/TodoHandler.cs
+++ Todo.Domain/Handlers/TodoHandler.cs
@@ -1,13 +1,17 @@
 ﻿using Todo.Domain.Repositories;
-using Flunt.Notifications;
 using Todo.Domain.Commands;
 using Todo.Domain.Commands.Contracts;
 using Todo.Domain.Handlers.Contracts;
 using Todo.Domain.Entities;
+using Flunt.Notifications;
 
 namespace Todo.Domain.Handlers;
 
-public class TodoHandler : Notifiable, IHandler<CreateTodoCommand>
+public class TodoHandler : Notifiable,
+    IHandler<CreateTodoCommand>,
+    IHandler<UpdateTodoCommand>,
+    IHandler<MarkTodoAsDone>,
+    IHandler<MarkTodoAsUndone>
 {
     private readonly ITodoRepository _repository;
 
@@ -50,5 +54,45 @@ public class TodoHandler : Notifiable, IHandler<CreateTodoCommand>
 
         // Retorno o resultado
         return new GenericCommandResult(true, "Tarefa Salva", todo);
+    }
+
+    public ICommandResult Handle(MarkTodoAsDone command)
+    {
+        // Fail, Fast, Validation
+        command.Validate();
+        if (command.Invalid)
+            return new GenericCommandResult(false, "Ops, parece que sua tarefa está errada", command.Notifications);
+
+        // Recupera o TodoItem
+        var todo = _repository.GetById(command.Id, command.User);
+
+        // Altera o estado
+        todo.MarkAsDone();
+
+        // Salva no banco
+        _repository.Update(todo);
+
+        // Retorna o TodoItem
+        return new GenericCommandResult(true, "Tarefa salva", todo);
+    }
+
+    public ICommandResult Handle(MarkTodoAsUndone command)
+    {
+        // Fail, Fast, validation
+        command.Validate();
+        if (command.Invalid)
+            return new GenericCommandResult(false, "Ops, parece que sua tarefa está errada", command.Notifications);
+
+        // Recupera o todoItem
+        var todo = _repository.GetById(command.Id, command.User);
+
+        // Altera o estado do todo
+        todo.MarkAsUndone();
+
+        // Salva no banco
+        _repository.Update(todo);
+
+        // Retorna o todoItem
+        return new GenericCommandResult(true, "Tarefa salva", todo);
     }
 }
