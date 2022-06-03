051082a8e8d58c0359205c74598477b50eb3f53a
Pedro Matheus <matt_4.6.8@outlook.com>
2022-06-03 09:04:51 -0300

    Iniciando os tests das Queries. Red, Green, Refactory. Fazendo ela falhar (Red)

---
+++ Todo.Domain.Tests/QueryTests/TodoQueryTests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Todo.Domain.Tests.QueryTests;
+
+[TestClass]
+public class TodoQueryTests
+{
+    [TestMethod]
+    public void Dada_a_consulta_retornar_tarefas_apenas_do_usuario_PedroMatheus()
+    {
+        Assert.Fail();
+    }
+}
