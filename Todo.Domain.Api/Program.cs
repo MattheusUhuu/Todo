bb134f3862b12f3aadf77090cbde3e2a93db5d39
Pedro Matheus <matt_4.6.8@outlook.com>
2022-06-05 13:53:56 -0300

    Criado a estrutura do TodoRepository e implementado os metodos update, create e todas as Queries

--- Todo.Domain.Api/Program.cs
+++ Todo.Domain.Api/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.EntityFrameworkCore;
+using Todo.Domain.Handlers;
 using Todo.Domain.Infra.Contexts;
+using Todo.Domain.Infra.Repositories;
+using Todo.Domain.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +11,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
 // builder.Services.AddDbContext<DataContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("connectionString")));
+builder.Services.AddTransient<ITodoRepository, TodoRepository>();
+builder.Services.AddTransient<TodoHandler, TodoHandler>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
